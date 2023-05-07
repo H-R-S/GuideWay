626498be4ac950ace0e66baf49fb8b33550edd0c
H-R-S <369haris@gmail.com>
2023-05-07 15:42:35 +0500

    New routes added | Routes Name

---
+++ lib/routes/routes_name.dart
@@ -0,0 +1,17 @@
+class RoutesName {
+
+  static const String splash = "splash_screen";
+
+  static const String onBoarding = "on_boarding_screen";
+
+  static const String login = "login_screen";
+
+  static const String signup = "sign_up_screen";
+
+  static const String forgotPassword = "forgot_password_screen";
+
+  static const String home = "home_screen";
+
+  static const String country = "country_screen";
+
+}
