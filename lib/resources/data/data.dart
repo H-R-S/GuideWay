ee3f6072a022b8484cffe7989e62a29174d917f1
H-R-S <369haris@gmail.com>
2023-05-07 15:41:40 +0500

    New data added | Data

---
+++ lib/resources/data/data.dart
@@ -0,0 +1,14 @@
+import 'package:guide_way/resources/constants/icons.dart';
+
+List<Map<String, dynamic>> data = [
+  {"title": "Colleges", "icon": collegeIcon, "isDisable": false},
+  {"title": "Weather", "icon": weatherIcon, "isDisable": false},
+  {"title": "Hotels", "icon": hotelIcon, "isDisable": false},
+  {"title": "Location", "icon": locationIcon, "isDisable": false},
+  {"title": "Currency", "icon": currencyExchangeIcon, "isDisable": false},
+  {"title": "Translate", "icon": translateIcon, "isDisable": false},
+  {"title": "Prayer Timings", "icon": prayerIcon, "isDisable": false},
+  {"title": "Qibla Direction", "icon": qiblaIcon, "isDisable": false},
+  {"title": "Resources", "icon": resourcesIcon, "isDisable": false},
+  {"title": "Jobs", "icon": jobsIcon, "isDisable": true}
+];
