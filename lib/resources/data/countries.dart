8fe1c1a6bf9e34a3d6daa27a8b45c80a458f5a77
H-R-S <369haris@gmail.com>
2023-05-07 15:41:55 +0500

    New countries list added | Countries Data

---
+++ lib/resources/data/countries.dart
@@ -0,0 +1,64 @@
+import '../constants/flags.dart';
+
+List<Map<String, dynamic>> countries = [
+  {
+    "id": 1,
+    "name": "USA",
+    "flag": usaFlag,
+    "dialingCode": "+1",
+  },
+  {
+    "id": 11,
+    "name": "Canada",
+    "flag": canadaFlag,
+    "dialingCode": "+1",
+  },
+  {
+    "id": 33,
+    "name": "France",
+    "flag": franceFlag,
+    "dialingCode": "+33",
+  },
+  {
+    "id": 44,
+    "name": "UK",
+    "flag": ukFlag,
+    "dialingCode": "+44",
+  },
+  {
+    "id": 46,
+    "name": "Sweden",
+    "flag": swedenFlag,
+    "dialingCode": "+46",
+  },
+  {
+    "id": 47,
+    "name": "Norway",
+    "flag": norwayFlag,
+    "dialingCode": "+47",
+  },
+  {
+    "id": 49,
+    "name": "Germany",
+    "flag": germanyFlag,
+    "dialingCode": "+49",
+  },
+  {
+    "id": 61,
+    "name": "Australia",
+    "flag": australiaFlag,
+    "dialingCode": "+61",
+  },
+  {
+    "id": 81,
+    "name": "Japan",
+    "flag": japanFlag,
+    "dialingCode": "+81",
+  },
+  {
+    "id": 82,
+    "name": "South Korea",
+    "flag": southKoreaFlag,
+    "dialingCode": "+82",
+  }
+];
