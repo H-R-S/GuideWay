0ea8f5bdfba1ade8d5a0ebb9c6dad3f43639d5ca
H-R-S <369haris@gmail.com>
2023-05-07 15:43:49 +0500

    Screen UI updated | SignUp Screen

---
+++ lib/view/screens/signup/sign_up_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:guide_way/routes/routes_name.dart';
+import '../../../resources/constants/icons.dart';
+import '../../widgets/button/my_circle_button.dart';
+import '../../widgets/button/my_elevated_button.dart';
+import '../../widgets/button/sign_in_button.dart';
+import '../../widgets/tag/main_tag.dart';
+import '../../widgets/text_field/my_text_field.dart';
+
+class SignUpScreen extends StatelessWidget {
+  SignUpScreen({super.key});
+
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            extendBodyBehindAppBar: true,
+            appBar: AppBar(
+                elevation: 0,
+                backgroundColor: Colors.transparent,
+                leading: BackButton(onPressed: () {
+                  Navigator.pop(context);
+                })),
+            body: SingleChildScrollView(
+                child: Padding(
+                    padding: const EdgeInsets.all(20).copyWith(top: 40),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const MainTag(
+                              title: "Sign Up now",
+                              subTitle:
+                                  "Please fill the details and create account"),
+                          const SizedBox(height: 40),
+                          MyTextField(
+                              controller: usernameController, hint: "Username"),
+                          MyTextField(
+                              controller: emailController, hint: "Email"),
+                          MyTextField(
+                              isPassword: true,
+                              isVisible: true,
+                              controller: passwordController,
+                              hint: "Password"),
+                          const SizedBox(height: 40),
+                          MyElevatedButton(
+                              title: "Sign Up",
+                              onTap: () {
+                                Navigator.pushReplacementNamed(
+                                    context, RoutesName.country);
+                              }),
+                          const SignInButton(),
+                          const SizedBox(height: 40),
+                          const Text("or continue with"),
+                          const SizedBox(height: 40),
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                MyCircleButton(icon: googleIcon, onTap: () {}),
+                                MyCircleButton(
+                                    icon: facebookIcon, onTap: () {}),
+                                MyCircleButton(icon: twitterIcon, onTap: () {})
+                              ])
+                        ])))));
+  }
+}
