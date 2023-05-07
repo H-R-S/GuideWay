8e7610d3dfe0de848e045f87ce4e8e11a3bece3a
H-R-S <369haris@gmail.com>
2023-05-07 15:40:28 +0500

    New icons added | Icons Constants

---
+++ lib/resources/constants/icons.dart
@@ -0,0 +1,30 @@
+const String facebookIcon = "assets/icons/facebook_icon.png";
+const String googleIcon = "assets/icons/google_icon.png";
+const String twitterIcon = "assets/icons/twitter_icon.png";
+const String searchIcon = "assets/icons/search_icon.png";
+const String activeSearchIcon = "assets/icons/active_search_icon.png";
+
+const String calenderIcon = "assets/icons/calender_icon.png";
+const String chatIcon = "assets/icons/chat_icon.png";
+const String prayerIcon = "assets/icons/prayer_icon.png";
+const String qiblaIcon = "assets/icons/qibla_direction_icon.png";
+const String resourcesIcon = "assets/icons/resources_icon.png";
+const String translateIcon = "assets/icons/translate_icon.png";
+const String collegeIcon = "assets/icons/university_icon.png";
+const String weatherIcon = "assets/icons/weather_icon.png";
+const String taskIcon = "assets/icons/task_icon.png";
+const String jobsIcon = "assets/icons/jobs_icon.png";
+const String currencyExchangeIcon = "assets/icons/currency_exchange_icon.png";
+const String hotelIcon = "assets/icons/hotel_icon.png";
+const String locationIcon = "assets/icons/location_icon.png";
+const String touristDestinationIcon = "assets/icons/tourist_destination_icon.png";
+const String personIcon = "assets/icons/person_icon.png";
+const String notificationIcon = "assets/icons/notification_icon.png";
+const String privacyPolicyIcon = "assets/icons/privacy_policy_icon.png";
+const String logoutIcon = "assets/icons/logout_icon.png";
+const String darkModeIcon = "assets/icons/dark_mode_icon.png";
+
+const String homeIcon = "assets/icons/home_icon.png";
+const String settingsIcon = "assets/icons/settings_icon.png";
+const String profileIcon = "assets/icons/profile_icon.png";
+const String scanIcon = "assets/icons/scan_icon.png";
