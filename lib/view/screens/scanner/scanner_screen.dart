ceae126aa645e1d8f40ef5730df1dbcb9129407f
H-R-S <369haris@gmail.com>
2023-05-15 23:48:21 +0500

    Screen UI Updated | Scanner Screen

---
+++ lib/view/screens/scanner/scanner_screen.dart
@@ -0,0 +1,191 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import '../../widgets/app_bar/my_app_bar.dart';
+
+class ScannerScreen extends StatefulWidget {
+  ScannerScreen({super.key});
+
+  @override
+  State<ScannerScreen> createState() => _ScannerScreenState();
+}
+
+class _ScannerScreenState extends State<ScannerScreen> with WidgetsBindingObserver {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+
+  bool _isPermissionGranted = false;
+
+  late final Future<void> _future;
+  CameraController? _cameraController;
+
+  // final textRecognizer = TextRecognizer();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+
+    _future = _requestCameraPermission();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    _stopCamera();
+    // textRecognizer.close();
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (_cameraController == null || !_cameraController!.value.isInitialized) {
+      return;
+    }
+
+    if (state == AppLifecycleState.inactive) {
+      _stopCamera();
+    } else if (state == AppLifecycleState.resumed &&
+        _cameraController != null &&
+        _cameraController!.value.isInitialized) {
+      _startCamera();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _future,
+      builder: (context, snapshot) {
+        return Stack(
+          children: [
+            if (_isPermissionGranted)
+              FutureBuilder<List<CameraDescription>>(
+                future: availableCameras(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    _initCameraController(snapshot.data!);
+
+                    return Center(child: CameraPreview(_cameraController!));
+                  } else {
+                    return const LinearProgressIndicator();
+                  }
+                },
+              ),
+            Scaffold(
+              appBar: MyAppBar(scaffoldKey, context, title: "Scan"),
+              backgroundColor: _isPermissionGranted ? Colors.transparent : null,
+              body: _isPermissionGranted
+                  ? Column(
+                      children: [
+                        Expanded(
+                          child: Container(),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(bottom: 30.0),
+                          child: Center(
+                            child: ElevatedButton(
+                              onPressed: _scanImage,
+                              child: const Text('Scan text'),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  : Center(
+                      child: Container(
+                        padding: const EdgeInsets.only(left: 24.0, right: 24.0),
+                        child: const Text(
+                          'Camera permission denied',
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _requestCameraPermission() async {
+    final status = await Permission.camera.request();
+    _isPermissionGranted = status == PermissionStatus.granted;
+  }
+
+  void _startCamera() {
+    if (_cameraController != null) {
+      _cameraSelected(_cameraController!.description);
+    }
+  }
+
+  void _stopCamera() {
+    if (_cameraController != null) {
+      _cameraController?.dispose();
+    }
+  }
+
+  void _initCameraController(List<CameraDescription> cameras) {
+    if (_cameraController != null) {
+      return;
+    }
+
+    // Select the first rear camera.
+    CameraDescription? camera;
+    for (var i = 0; i < cameras.length; i++) {
+      final CameraDescription current = cameras[i];
+      if (current.lensDirection == CameraLensDirection.back) {
+        camera = current;
+        break;
+      }
+    }
+
+    if (camera != null) {
+      _cameraSelected(camera);
+    }
+  }
+
+  Future<void> _cameraSelected(CameraDescription camera) async {
+    _cameraController = CameraController(
+      camera,
+      ResolutionPreset.max,
+      enableAudio: false,
+    );
+
+    await _cameraController!.initialize();
+    await _cameraController!.setFlashMode(FlashMode.off);
+
+    if (!mounted) {
+      return;
+    }
+    setState(() {});
+  }
+
+  Future<void> _scanImage() async {
+    if (_cameraController == null) return;
+
+    // final navigator = Navigator.of(context);
+
+    try {
+      // final pictureFile = await _cameraController!.takePicture();
+
+      // final file = File(pictureFile.path);
+
+      // final inputImage = InputImage.fromFile(file);
+      // final recognizedText = await textRecognizer.processImage("inputImage");
+
+      // await navigator.push(
+      //   MaterialPageRoute(
+      //     builder: (BuildContext context) =>
+      //         Container(text: recognizedText.text),
+      //   ),
+      // );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('An error occurred when scanning text'),
+        ),
+      );
+    }
+  }
+}
