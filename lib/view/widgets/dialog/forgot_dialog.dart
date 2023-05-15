7cfc936ea61b8d5738def46a03f8d2ad54f3d891
H-R-S <369haris@gmail.com>
2023-05-15 23:55:51 +0500

    Widget UI Updated | ForgotDialog Widget

--- lib/view/widgets/dialog/forgot_dialog.dart
+++ lib/view/widgets/dialog/forgot_dialog.dart
@@ -5,30 +5,28 @@ class ForgotDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Dialog(
-        elevation: 0,
-        child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: SizedBox(
-                width: double.infinity,
-                child: Column(mainAxisSize: MainAxisSize.min, children: const [
-                  CircleAvatar(
-                      radius: 40,
-                      backgroundColor: Colors.blue,
-                      child: Icon(Icons.mark_email_read_outlined,
-                          size: 50, color: Colors.white)),
-                  SizedBox(height: 20),
-                  Text("Check your email",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-                  SizedBox(height: 20),
-                  Text(
-                      "We have sent password recovery instruction to your email",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontWeight: FontWeight.normal,
-                          color: Colors.black54,
-                          fontSize: 18))
-                ]))));
+    return const Padding(
+        padding: EdgeInsets.all(20),
+        child: SizedBox(
+            width: double.infinity,
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+              CircleAvatar(
+                  radius: 40,
+                  backgroundColor: Colors.blue,
+                  child: Icon(Icons.mark_email_read_outlined,
+                      size: 50, color: Colors.white)),
+              SizedBox(height: 20),
+              Text("Check your email",
+                  style:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+              SizedBox(height: 20),
+              Text(
+                  "We have sent password recovery instruction to your email",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      color: Colors.black54,
+                      fontSize: 18))
+            ])));
   }
 }
