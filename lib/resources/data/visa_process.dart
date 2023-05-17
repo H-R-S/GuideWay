e97c8d822c741166f0bd862919ad12aa9d33d819
H-R-S <369haris@gmail.com>
2023-05-17 23:12:24 +0500

    New dummy data added | Visa Process

---
+++ lib/resources/data/visa_process.dart
@@ -0,0 +1,29 @@
+import '../constants/icons.dart';
+
+List<Map<String, dynamic>> visaProcess = [
+  {
+    "title": "Visa Fee",
+    "subTitle": "100\$",
+    "icon": visaIcon
+  },
+  {
+    "title": "Embassy Location",
+    "subTitle": "Karachi, Pakistan",
+    "icon": embassyIcon
+  },
+  {
+    "title": "Visa Process Time",
+    "subTitle": "1-3 month",
+    "icon": visaProcessIcon
+  },
+  {
+    "title": "Interview Required for Visa",
+    "subTitle": "No",
+    "icon": resourcesIcon
+  },
+  {
+    "title": "Chances to Get Visa",
+    "subTitle": "75%",
+    "icon": visaChanceIcon
+  }
+];
