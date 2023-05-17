b9022c5ebf398cb20a7a438855d063bf8217979a
H-R-S <369haris@gmail.com>
2023-05-17 23:11:51 +0500

    New icons added | Icon Constants

--- lib/resources/constants/icons.dart
+++ lib/resources/constants/icons.dart
@@ -1,3 +1,5 @@
+const String guideWayIcon = "assets/icons/guide_way_icon.png";
+
 const String facebookIcon = "assets/icons/facebook_icon.png";
 const String googleIcon = "assets/icons/google_icon.png";
 const String twitterIcon = "assets/icons/twitter_icon.png";
@@ -5,7 +7,6 @@ const String searchIcon = "assets/icons/search_icon.png";
 const String activeSearchIcon = "assets/icons/active_search_icon.png";
 
 const String calenderIcon = "assets/icons/calender_icon.png";
-const String chatIcon = "assets/icons/chat_icon.png";
 const String prayerIcon = "assets/icons/prayer_icon.png";
 const String qiblaIcon = "assets/icons/qibla_direction_icon.png";
 const String resourcesIcon = "assets/icons/resources_icon.png";
@@ -23,8 +24,13 @@ const String notificationIcon = "assets/icons/notification_icon.png";
 const String privacyPolicyIcon = "assets/icons/privacy_policy_icon.png";
 const String logoutIcon = "assets/icons/logout_icon.png";
 const String darkModeIcon = "assets/icons/dark_mode_icon.png";
+const String visaChanceIcon = "assets/icons/visa_chance_icon.png";
+const String visaIcon = "assets/icons/visa_icon.png";
+const String visaProcessIcon = "assets/icons/visa_process_time_icon.png";
+const String embassyIcon = "assets/icons/embassy_icon.png";
 
 const String homeIcon = "assets/icons/home_icon.png";
+const String scanIcon = "assets/icons/scan_icon.png";
+const String chatIcon = "assets/icons/chat_icon.png";
 const String settingsIcon = "assets/icons/settings_icon.png";
 const String profileIcon = "assets/icons/profile_icon.png";
-const String scanIcon = "assets/icons/scan_icon.png";
