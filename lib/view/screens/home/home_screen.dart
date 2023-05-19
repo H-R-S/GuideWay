67249d9ae81a23205a243c378a6730345cf20791
H-R-S <369haris@gmail.com>
2023-05-19 23:12:12 +0500

    Screen UI updated | Home Screen

--- lib/view/screens/home/home_screen.dart
+++ lib/view/screens/home/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:guide_way/view/widgets/app_bar/my_app_bar.dart';
 import '../../../resources/data/data.dart';
+import '../../widgets/app_bar/my_app_bar.dart';
 import '../../widgets/info_container/info_container.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -15,29 +15,32 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
         appBar: MyAppBar(scaffoldKey, context, isHome: true),
         body: SingleChildScrollView(
-          child: Padding(
-              padding: const EdgeInsets.all(20).copyWith(top: 40),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    GridView.builder(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                            mainAxisExtent: width * 0.4,
-                            crossAxisSpacing: 30,
-                            mainAxisSpacing: 30,
-                            crossAxisCount: 2),
-                        itemCount: data.length,
-                        itemBuilder: (context, index) {
-                          return InfoContainer(
-                              isDisable: data[index]["isDisable"],
-                              title: data[index]["title"],
-                              icon: data[index]["icon"],
-                              onTap: () {});
-                        }),
-                    const SizedBox(height: 40)
-                  ])),
-        ));
+            child: Padding(
+                padding: const EdgeInsets.all(20).copyWith(top: 40),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      GridView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                                  mainAxisExtent: width * 0.4,
+                                  crossAxisSpacing: 30,
+                                  mainAxisSpacing: 30,
+                                  crossAxisCount: 2),
+                          itemCount: data.length,
+                          itemBuilder: (context, index) {
+                            return InfoContainer(
+                                isDisable: data[index]["isDisable"],
+                                title: data[index]["title"],
+                                icon: data[index]["icon"],
+                                onTap: () {
+                                  Navigator.pushNamed(
+                                      context, data[index]["route"]);
+                                });
+                          }),
+                      const SizedBox(height: 40)
+                    ]))));
   }
 }
