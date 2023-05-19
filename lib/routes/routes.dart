4ceb1c3d1624eac5191fd1092e85997b434e2b7d
H-R-S <369haris@gmail.com>
2023-05-19 23:11:11 +0500

    New routes added | Routes

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import '../home.dart';
 import '../view/screens/country/country_screen.dart';
+import '../view/screens/currency/currency_screen.dart';
+import '../view/screens/destinations/destinations_screen.dart';
 import '../view/screens/forgot_password/forgot_password_screen.dart';
+import '../view/screens/location/location_screen.dart';
 import '../view/screens/login/login_screen.dart';
 import '../view/screens/on_boarding/on_boarding_screen.dart';
+import '../view/screens/prayer_timings/prayer_timings_screen.dart';
+import '../view/screens/qibla_direction/qibla_direction_screen.dart';
 import '../view/screens/signup/sign_up_screen.dart';
+import '../view/screens/translate/translate_screen.dart';
+import '../view/screens/universities/universities_screen.dart';
+import '../view/screens/weather/weather_screen.dart';
 import 'routes_name.dart';
 
 class Routes {
@@ -27,6 +35,30 @@ class Routes {
 
       case RoutesName.country:
         return getPageRoute(const CountryScreen());
+      
+      case RoutesName.universities:
+        return getPageRoute(UniversitiesScreen());
+
+      case RoutesName.weather:
+        return getPageRoute(WeatherScreen());
+
+      case RoutesName.destinations:
+        return getPageRoute(DestinationsScreen());
+
+      case RoutesName.location:
+        return getPageRoute(LocationScreen());
+
+      case RoutesName.currency:
+        return getPageRoute(CurrencyScreen());
+
+      case RoutesName.translate:
+        return getPageRoute(TranslateScreen());
+
+      case RoutesName.prayerTimings:
+        return getPageRoute(PrayerTimingsScreen());
+
+      case RoutesName.qiblaDirection:
+        return getPageRoute(QiblaDirectionScreen());
 
       default:
         return getPageRoute(LoginScreen());
