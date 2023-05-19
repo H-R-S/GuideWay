231217f4c147778d0878bc0ccf6cc702e7fed8e1
H-R-S <369haris@gmail.com>
2023-05-19 23:10:22 +0500

    Data updated | Data

--- lib/resources/data/data.dart
+++ lib/resources/data/data.dart
@@ -1,14 +1,55 @@
 import 'package:guide_way/resources/constants/icons.dart';
+import 'package:guide_way/routes/routes_name.dart';
 
 List<Map<String, dynamic>> data = [
-  {"title": "Colleges", "icon": collegeIcon, "isDisable": false},
-  {"title": "Weather", "icon": weatherIcon, "isDisable": false},
-  {"title": "Hotels", "icon": hotelIcon, "isDisable": false},
-  {"title": "Location", "icon": locationIcon, "isDisable": false},
-  {"title": "Currency", "icon": currencyExchangeIcon, "isDisable": false},
-  {"title": "Translate", "icon": translateIcon, "isDisable": false},
-  {"title": "Prayer Timings", "icon": prayerIcon, "isDisable": false},
-  {"title": "Qibla Direction", "icon": qiblaIcon, "isDisable": false},
-  {"title": "Resources", "icon": resourcesIcon, "isDisable": false},
-  {"title": "Jobs", "icon": jobsIcon, "isDisable": true}
+  {
+    "route": RoutesName.universities,
+    "title": "Universities",
+    "icon": collegeIcon,
+    "isDisable": false
+  },
+  {
+    "route": RoutesName.weather,
+    "title": "Weather",
+    "icon": weatherIcon,
+    "isDisable": false
+  },
+  {
+    "route": RoutesName.destinations,
+    "title": "Destinations",
+    "icon": touristDestinationIcon,
+    "isDisable": false
+  },
+  {
+    "route": RoutesName.location,
+    "title": "Location",
+    "icon": locationIcon,
+    "isDisable": false
+  },
+  {
+    "route": RoutesName.currency,
+    "title": "Currency",
+    "icon": currencyExchangeIcon,
+    "isDisable": false
+  },
+  {
+    "route": RoutesName.translate,
+    "title": "Translate",
+    "icon": translateIcon,
+    "isDisable": false
+  },
+  {
+    "route": RoutesName.prayerTimings,
+    "title": "Prayer Timings",
+    "icon": prayerIcon,
+    "isDisable": false
+  },
+  {
+    "route": RoutesName.qiblaDirection,
+    "title": "Qibla Direction",
+    "icon": qiblaIcon,
+    "isDisable": false
+  }
+  // {"title": "Resources", "icon": resourcesIcon, "isDisable": false},
+  // {"title": "Jobs", "icon": jobsIcon, "isDisable": true}
 ];
