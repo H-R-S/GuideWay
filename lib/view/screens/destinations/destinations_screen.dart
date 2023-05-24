a728e6ccd78dcfce0c2198f0b74a08ce5033a4d6
H-R-S <369haris@gmail.com>
2023-05-24 23:30:17 +0500

    Screen UI Updated | Destinations Screen

--- lib/view/screens/destinations/destinations_screen.dart
+++ lib/view/screens/destinations/destinations_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:guide_way/resources/data/destinations.dart';
+import 'package:guide_way/view/widgets/image_container/image_container.dart';
 import '../../widgets/app_bar/my_app_bar.dart';
 
 class DestinationsScreen extends StatelessWidget {
@@ -9,7 +11,22 @@ class DestinationsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(scaffoldKey, context, title: "Destionations"),
-    );
+        appBar: MyAppBar(scaffoldKey, context, title: "Destionations"),
+        body: SingleChildScrollView(
+            child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(children: [
+                  ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: destinations.length,
+                      itemBuilder: (context, index) {
+                        return ImageContainer(
+                            name: destinations[index]["name"],
+                            description: destinations[index]["description"],
+                            image: destinations[index]["image"],
+                            onTap: () {});
+                      })
+                ]))));
   }
 }
