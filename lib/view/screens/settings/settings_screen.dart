d79090e39ef5bcf3f25831f168080238c4d24980
H-R-S <369haris@gmail.com>
2023-05-24 23:28:05 +0500

    Screen UI Updated | Settings Screen

--- lib/view/screens/settings/settings_screen.dart
+++ lib/view/screens/settings/settings_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:guide_way/resources/constants/icons.dart';
-import 'package:guide_way/routes/routes_name.dart';
-import 'package:guide_way/view/widgets/app_bar/my_app_bar.dart';
+import '../../../resources/constants/flags.dart';
+import '../../../resources/constants/icons.dart';
+import '../../../routes/routes_name.dart';
+import '../../widgets/app_bar/my_app_bar.dart';
 import '../../widgets/profile_container/profile_container.dart';
 import '../../widgets/settings_container/settings_container.dart';
 
@@ -14,34 +15,45 @@ class SettingsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: MyAppBar(scaffoldKey, context, title: "Settings"),
-        body: Column(children: [
-          ProfileContainer(
-              name: "Anus Ali Siddiqui",
-              userName: "Anus",
-              email: "anusali@gmail.com",
-              onTap: () {}),
-          SettingsContainer(
-              icon: logoutIcon,
-              title: "Logout",
-              subTitle: "Logout from the App",
-              onTap: () {
-                Navigator.pushReplacementNamed(context, RoutesName.login);
-              }),
-          SettingsContainer(
-              icon: notificationIcon,
-              title: "Notification",
-              subTitle: "Get the Latest Update Notification",
-              onTap: () {}),
-          SettingsContainer(
-              icon: darkModeIcon,
-              title: "Dark Mode",
-              subTitle: "Switch to Dark Mode",
-              onTap: () {}),
-          SettingsContainer(
-              icon: privacyPolicyIcon,
-              title: "Privacy Policy",
-              subTitle: "GuideWay Privacy Policy",
-              onTap: () {}),
-        ]));
+        body: SingleChildScrollView(
+          child: Column(children: [
+            ProfileContainer(
+                name: "Anus Ali Siddiqui",
+                userName: "Anus",
+                email: "anusali@gmail.com",
+                onTap: () {}),
+            SettingsContainer(
+                icon: logoutIcon,
+                title: "Logout",
+                subTitle: "Logout from the App",
+                onTap: () {
+                  Navigator.pushReplacementNamed(context, RoutesName.login);
+                }),
+            SettingsContainer(
+                icon: germanyFlag,
+                title: "Country",
+                subTitle: "Change your desire country",
+                onTap: () {
+                  Navigator.pushReplacementNamed(context, RoutesName.country);
+                }),
+            SettingsContainer(
+                icon: notificationIcon,
+                title: "Notification",
+                subTitle: "Get the Latest Update Notification",
+                onTap: () {}),
+            SettingsContainer(
+                isSwitch: true,
+                icon: darkModeIcon,
+                title: "Dark Mode",
+                subTitle: "Switch to Dark Mode",
+                onTap: () {}),
+            SettingsContainer(
+                icon: privacyPolicyIcon,
+                title: "Privacy Policy",
+                subTitle: "GuideWay Privacy Policy",
+                onTap: () {}),
+            const SizedBox(height: 40)
+          ]),
+        ));
   }
 }
