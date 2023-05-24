72f42e28de8ea539512bdef61032ac2fa2a6ef00
H-R-S <369haris@gmail.com>
2023-05-24 23:32:51 +0500

    Dummy data added | Universities

---
+++ lib/resources/data/universities.dart
@@ -0,0 +1,24 @@
+import 'package:guide_way/resources/constants/images.dart';
+
+List<Map<String, dynamic>> universities = [
+  {
+    "name": "University of Oxford",
+    "city": "Oxford",
+    "image": oxfordUniversityImage
+  },
+  {
+    "name": "University of Cambridge",
+    "city": "Cambridge",
+    "image": cambridgeUniversityImage
+  },
+  {
+    "name": "Imperial College London",
+    "city": "London",
+    "image": imperialCollegeUniversityImage
+  },
+  {
+    "name": "University College London (UCL)",
+    "city": "London",
+    "image": universityCollegeImage
+  }
+];
