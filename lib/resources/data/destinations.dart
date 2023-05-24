09ad830cf39db029b5cff87f743075ff0933793e
H-R-S <369haris@gmail.com>
2023-05-24 23:33:01 +0500

    Dummy data added | Destinations

---
+++ lib/resources/data/destinations.dart
@@ -0,0 +1,24 @@
+import 'package:guide_way/resources/constants/images.dart';
+
+List<Map<String, dynamic>> destinations = [
+  {
+    "name": "London",
+    "description": "The capital city of England is one of the world's most visited cities, offering a rich history, diverse cultural experiences, and world-class museums and galleries.",
+    "image": londonImage
+  },
+  {
+    "name": "Edinburgh",
+    "description": "The capital city of Scotland is a UNESCO World Heritage Site, featuring stunning architecture, a vibrant arts scene, and the famous Edinburgh Castle.",
+    "image": edinburghImage
+  },
+  {
+    "name": "Stonehenge",
+    "description": "A prehistoric monument in Wiltshire, England, consisting of large stones arranged in a circular formation, which has fascinated visitors for centuries.",
+    "image": stonehengeImage
+  },
+  {
+    "name": "The Lake District",
+    "description": "A beautiful region in North West England, known for its scenic lakes, mountains, and hiking trails.",
+    "image": lakeDistrictImage
+  }
+];
