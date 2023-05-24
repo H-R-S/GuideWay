8744352027df136e1f7fa0b143d1e0538f671be5
H-R-S <369haris@gmail.com>
2023-05-24 23:25:11 +0500

    Widget UI Updated | InfoContainer Widget

--- lib/view/widgets/info_container/info_container.dart
+++ lib/view/widgets/info_container/info_container.dart
@@ -16,39 +16,27 @@ class InfoContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: isDisable ? null : onTap,
-      child: Container(
-          padding: const EdgeInsets.all(20),
-          decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: const [
-                BoxShadow(
-                    color: Colors.grey,
-                    spreadRadius: 1,
-                    blurRadius: 5,
-                    offset: Offset(0, 4))
-              ],
-              borderRadius: BorderRadius.circular(20)),
-          child: FittedBox(
-              child: Column(children: [
-            Stack(alignment: Alignment.center, children: [
-              if (isDisable)
-                const Positioned(
-                    child: Text("Coming Soon",
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold))),
-              Opacity(
-                  opacity: isDisable ? 0.3 : 1,
-                  child: Image.asset(icon, height: 100, fit: BoxFit.fitHeight)),
-            ]),
-            const SizedBox(height: 10),
-            Opacity(
-              opacity: isDisable ? 0.3 : 1,
-              child: Text(title,
+        onTap: isDisable ? null : onTap,
+        child: Container(
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: const [
+                  BoxShadow(
+                      color: Colors.grey,
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: Offset(0, 4))
+                ],
+                borderRadius: BorderRadius.circular(20)),
+            child: FittedBox(
+                child: Column(children: [
+              Image.asset(icon, height: 100, width: 100),
+              const SizedBox(height: 10),
+              Text(title,
+                  textAlign: TextAlign.center,
                   style: const TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold)),
-            )
-          ]))),
-    );
+                      fontSize: 18, fontWeight: FontWeight.bold))
+            ]))));
   }
 }
