f64a0eae6db68d321f5156ed27477a9822f53eba
H-R-S <369haris@gmail.com>
2023-05-24 23:32:36 +0500

    Dummy data added | Home Data

--- lib/resources/data/data.dart
+++ lib/resources/data/data.dart
@@ -1,55 +1,65 @@
-import 'package:guide_way/resources/constants/icons.dart';
-import 'package:guide_way/routes/routes_name.dart';
+import '../../routes/routes_name.dart';
+import '../constants/icons.dart';
 
 List<Map<String, dynamic>> data = [
   {
     "route": RoutesName.universities,
     "title": "Universities",
     "icon": collegeIcon,
-    "isDisable": false
-  },
-  {
-    "route": RoutesName.weather,
-    "title": "Weather",
-    "icon": weatherIcon,
-    "isDisable": false
   },
   {
     "route": RoutesName.destinations,
     "title": "Destinations",
     "icon": touristDestinationIcon,
-    "isDisable": false
+  },
+  {
+    "route": RoutesName.weather,
+    "title": "Weather",
+    "icon": weatherIcon,
   },
   {
     "route": RoutesName.location,
     "title": "Location",
     "icon": locationIcon,
-    "isDisable": false
   },
   {
     "route": RoutesName.currency,
     "title": "Currency",
     "icon": currencyExchangeIcon,
-    "isDisable": false
   },
   {
     "route": RoutesName.translate,
     "title": "Translate",
     "icon": translateIcon,
-    "isDisable": false
+  },
+  {
+    "route": RoutesName.destinations,
+    "title": "Job Timings",
+    "icon": jobsIcon,
+  },
+  {
+    "route": RoutesName.destinations,
+    "title": "Transport Rules",
+    "icon": transportIcon,
+  },
+  {
+    "route": RoutesName.destinations,
+    "title": "Airport Rules",
+    "icon": airportIcon,
+  },
+  {
+    "route": RoutesName.destinations,
+    "title": "Driving Rules",
+    "icon": drivingIcon,
   },
   {
     "route": RoutesName.prayerTimings,
     "title": "Prayer Timings",
     "icon": prayerIcon,
-    "isDisable": false
   },
   {
     "route": RoutesName.qiblaDirection,
     "title": "Qibla Direction",
     "icon": qiblaIcon,
-    "isDisable": false
   }
-  // {"title": "Resources", "icon": resourcesIcon, "isDisable": false},
-  // {"title": "Jobs", "icon": jobsIcon, "isDisable": true}
 ];
