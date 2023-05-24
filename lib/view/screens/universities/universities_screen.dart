a9950e2b44921a5ce52919037b9191531776f752
H-R-S <369haris@gmail.com>
2023-05-24 23:27:09 +0500

    Screen UI Updated | Universities Screen

--- lib/view/screens/universities/universities_screen.dart
+++ lib/view/screens/universities/universities_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:guide_way/resources/data/universities.dart';
 import '../../widgets/app_bar/my_app_bar.dart';
+import '../../widgets/image_container/image_container.dart';
 
 class UniversitiesScreen extends StatelessWidget {
   UniversitiesScreen({super.key});
@@ -9,7 +11,22 @@ class UniversitiesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(scaffoldKey, context, title: "Universities"),
-    );
+        appBar: MyAppBar(scaffoldKey, context, title: "Universities"),
+        body: SingleChildScrollView(
+            child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(children: [
+                  ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: universities.length,
+                      itemBuilder: (context, index) {
+                        return ImageContainer(
+                            name: universities[index]["name"],
+                            city: universities[index]["city"],
+                            image: universities[index]["image"],
+                            onTap: () {});
+                      })
+                ]))));
   }
 }
