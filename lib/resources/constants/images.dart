e70ad092c428b2b7c32aed0c70c93d15b32cac1a
H-R-S <369haris@gmail.com>
2023-05-24 23:31:47 +0500

    New images added | Images Constants

--- lib/resources/constants/images.dart
+++ lib/resources/constants/images.dart
@@ -1,2 +1,12 @@
 const String onBoarding01 = "assets/images/on_boarding_01.jpg";
 const String onBoarding02 = "assets/images/on_boarding_02.png";
+
+const String londonImage = "assets/images/london_image.jpg";
+const String edinburghImage = "assets/images/edinburgh_image.jpg";
+const String stonehengeImage = "assets/images/stonehenge_image.jpg";
+const String lakeDistrictImage = "assets/images/lake_district_image.jpg";
+
+const String cambridgeUniversityImage = "assets/images/cambridge_university_image.jpg";
+const String imperialCollegeUniversityImage = "assets/images/imperial_college_university_image.jpg";
+const String oxfordUniversityImage = "assets/images/oxford_university_image.jpg";
+const String universityCollegeImage = "assets/images/university_college_image.jpg";
