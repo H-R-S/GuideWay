94ce3448409618a5558060fa59b6751315bf8a28
H-R-S <369haris@gmail.com>
2023-05-25 23:21:39 +0500

    New dummy data added | PrayerTime

---
+++ lib/resources/data/prayer_time.dart
@@ -0,0 +1,7 @@
+List<Map<String, dynamic>> prayerTime = [
+  {"name": "Fajr", "azanTime": "04:30 AM", "iqamaTime": "05:15 AM"},
+  {"name": "Dhur", "azanTime": "12:45 PM", "iqamaTime": "01:15 PM"},
+  {"name": "Asr", "azanTime": "05:10 PM", "iqamaTime": "05:45 PM"},
+  {"name": "Maghrib", "azanTime": "07:15 PM", "iqamaTime": "07:20 PM"},
+  {"name": "Isha", "azanTime": "08:45 PM", "iqamaTime": "09:00 PM"}
+];
