0eb0f6680b0fd599dbede6a111c0f94ace449178
H-R-S <369haris@gmail.com>
2023-05-25 23:23:10 +0500

    Screen UI Updated | Prayer Timing Screen

--- lib/view/screens/prayer_timings/prayer_timings_screen.dart
+++ lib/view/screens/prayer_timings/prayer_timings_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:guide_way/resources/data/prayer_time.dart';
+import 'package:guide_way/view/widgets/prayer_time_container/prayer_time_container.dart';
 import '../../widgets/app_bar/my_app_bar.dart';
 
 class PrayerTimingsScreen extends StatelessWidget {
@@ -9,7 +11,21 @@ class PrayerTimingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(scaffoldKey, context, title: "Prayer Timings"),
-    );
+        appBar: MyAppBar(scaffoldKey, context, title: "Prayer Timings"),
+        body: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(children: [
+              const PrayerTimeContainer(isHeader: true),
+              ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: prayerTime.length,
+                  itemBuilder: (context, index) {
+                    return PrayerTimeContainer(
+                        name: prayerTime[index]["name"],
+                        azanTime: prayerTime[index]["azanTime"],
+                        iqamaTime: prayerTime[index]["iqamaTime"]);
+                  })
+            ])));
   }
 }
