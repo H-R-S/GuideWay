82e39186d4eeadb3501f7ff53973f630f27e725d
H-R-S <369haris@gmail.com>
2023-05-26 21:19:01 +0500

    Screen UI Updated | Settings Screen

--- lib/view/screens/settings/settings_screen.dart
+++ lib/view/screens/settings/settings_screen.dart
@@ -16,44 +16,48 @@ class SettingsScreen extends StatelessWidget {
     return Scaffold(
         appBar: MyAppBar(scaffoldKey, context, title: "Settings"),
         body: SingleChildScrollView(
-          child: Column(children: [
-            ProfileContainer(
-                name: "Anus Ali Siddiqui",
-                userName: "Anus",
-                email: "anusali@gmail.com",
-                onTap: () {}),
-            SettingsContainer(
-                icon: logoutIcon,
-                title: "Logout",
-                subTitle: "Logout from the App",
-                onTap: () {
-                  Navigator.pushReplacementNamed(context, RoutesName.login);
-                }),
-            SettingsContainer(
-                icon: germanyFlag,
-                title: "Country",
-                subTitle: "Change your desire country",
-                onTap: () {
-                  Navigator.pushReplacementNamed(context, RoutesName.country);
-                }),
-            SettingsContainer(
-                icon: notificationIcon,
-                title: "Notification",
-                subTitle: "Get the Latest Update Notification",
-                onTap: () {}),
-            SettingsContainer(
-                isSwitch: true,
-                icon: darkModeIcon,
-                title: "Dark Mode",
-                subTitle: "Switch to Dark Mode",
-                onTap: () {}),
-            SettingsContainer(
-                icon: privacyPolicyIcon,
-                title: "Privacy Policy",
-                subTitle: "GuideWay Privacy Policy",
-                onTap: () {}),
-            const SizedBox(height: 40)
-          ]),
-        ));
+            child: Column(children: [
+          ProfileContainer(
+              name: "Anus Ali Siddiqui",
+              userName: "Anus",
+              email: "anusali@gmail.com",
+              onTap: () {}),
+          SettingsContainer(
+              icon: logoutIcon,
+              title: "Logout",
+              subTitle: "Logout from the App",
+              onTap: () {
+                Navigator.pushReplacementNamed(context, RoutesName.login);
+              }),
+          SettingsContainer(
+              icon: germanyFlag,
+              title: "Country",
+              subTitle: "Change your desire country",
+              onTap: () {
+                Navigator.pushReplacementNamed(context, RoutesName.country);
+              }),
+          SettingsContainer(
+              icon: notificationIcon,
+              title: "Notification",
+              subTitle: "Get the Latest Update Notification",
+              onTap: () {}),
+          SettingsContainer(
+              isSwitch: true,
+              icon: darkModeIcon,
+              title: "Dark Mode",
+              subTitle: "Switch to Dark Mode",
+              onTap: () {}),
+          SettingsContainer(
+              icon: privacyPolicyIcon,
+              title: "Privacy Policy",
+              subTitle: "GuideWay Privacy Policy",
+              onTap: () {}),
+          SettingsContainer(
+              icon: termsAndConditionIcon,
+              title: "Terms & Conditions",
+              subTitle: "GuideWay Terms & Conditions",
+              onTap: () {}),
+          const SizedBox(height: 50)
+        ])));
   }
 }
