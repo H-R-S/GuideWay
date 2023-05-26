733e9ad4e784b696840f5807032289b950bcbcdb
H-R-S <369haris@gmail.com>
2023-05-26 21:19:31 +0500

    Screen UI Updated | Qibla Direction Screen

--- lib/view/screens/qibla_direction/qibla_direction_screen.dart
+++ lib/view/screens/qibla_direction/qibla_direction_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:guide_way/resources/constants/icons.dart';
 import '../../widgets/app_bar/my_app_bar.dart';
 
 class QiblaDirectionScreen extends StatelessWidget {
@@ -9,7 +10,19 @@ class QiblaDirectionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(scaffoldKey, context, title: "Qibla"),
-    );
+        appBar: MyAppBar(scaffoldKey, context, title: "Qibla Direction"),
+        body: Padding(
+            padding: const EdgeInsets.all(20).copyWith(top: 40),
+            child: Column(children: [
+              const Text("0°",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 20),
+              Container(
+                  decoration: BoxDecoration(
+                      color: Colors.grey.shade400, shape: BoxShape.circle),
+                  child: Padding(
+                      padding: const EdgeInsets.all(10).copyWith(bottom: 3),
+                      child: Image.asset(qiblaCompasIcon)))
+            ])));
   }
 }
