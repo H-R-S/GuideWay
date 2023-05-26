c6c3d6b6c4e42fb91dfb91cac9d0ffab345bee03
H-R-S <369haris@gmail.com>
2023-05-26 21:17:25 +0500

    Screen UI Updated | Airport Rules Screen

--- lib/view/screens/airport_rules/airport_rules_screen.dart
+++ lib/view/screens/airport_rules/airport_rules_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:guide_way/resources/data/rules.dart';
+import '../../widgets/app_bar/my_app_bar.dart';
 
 class AirportRulesScreen extends StatelessWidget {
-  const AirportRulesScreen({super.key});
+  AirportRulesScreen({super.key});
+
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    const style = TextStyle(fontSize: 18, fontWeight: FontWeight.normal);
+
+    return Scaffold(
+        appBar: MyAppBar(scaffoldKey, context, title: "Airport Rules"),
+        body: SingleChildScrollView(
+            child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(children: [
+                  Text(airportRules[0]["description"], style: style),
+                  const SizedBox(height: 20),
+                  Text(airportRules[0]["rules"], style: style)
+                ]))));
   }
 }
