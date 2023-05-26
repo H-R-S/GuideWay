d82ff2fe05dcf1ab68cc9e21f0a1c176f689bf96
H-R-S <369haris@gmail.com>
2023-05-26 21:18:45 +0500

    Screen UI Updated | Location Screen

--- lib/view/screens/location/location_screen.dart
+++ lib/view/screens/location/location_screen.dart
@@ -1,15 +1,36 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../../widgets/app_bar/my_app_bar.dart';
 
-class LocationScreen extends StatelessWidget {
-  LocationScreen({super.key});
+class LocationScreen extends StatefulWidget {
+  const LocationScreen({super.key});
 
+  @override
+  State<LocationScreen> createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(24.8395749, 67.0823143),
+    zoom: 14.4746,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(scaffoldKey, context, title: "Location"),
-    );
+        appBar: MyAppBar(scaffoldKey, context, title: "Location"),
+        body: GoogleMap(
+            mapType: MapType.hybrid,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            }
+        ));
   }
 }
