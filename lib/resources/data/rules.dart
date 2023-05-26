9e60ab726eca33f404c75837eff69e8604a47a5b
H-R-S <369haris@gmail.com>
2023-05-26 21:15:31 +0500

    New dummy data added | Rules

---
+++ lib/resources/data/rules.dart
@@ -0,0 +1,49 @@
+List<Map<String, dynamic>> transportRules = [
+  {
+    "description":
+        "In the UK, students who use public transport to get to and from school or university are expected to follow a set of rules to ensure their safety and the safety of others. Some of the general rules that apply to the transport sector for students in the UK include",
+    "rules": """
+1.	Always wear a face covering while using public transport, as required by law.
+2.	Use hand sanitizer before and after using public transport, to help prevent the spread of germs.
+3.	Observe social distancing guidelines while on public transport, keeping a safe distance from other passengers where possible.
+4.	Always follow the instructions of transport staff, including drivers and conductors.
+5.	Refrain from eating, drinking or smoking while on public transport.
+6.	Be considerate of other passengers, including those who may have disabilities or special needs.
+7.	Pay attention to announcements and information displayed on public transport, such as timetables and route maps.
+8.	Be aware of your surroundings and report any suspicious behavior or items to transport staff or the police.
+9.	Avoid playing loud music or causing disturbances that may disrupt other passengers.
+10.	Always carry a valid ticket or pass when using public transport, and be prepared to show it to transport staff when requested."
+"""
+  }
+];
+
+List<Map<String, dynamic>> drivingRules = [
+  {
+    "description":
+        "In the UK, driving is done on the left side of the road, and the minimum age for driving a car is 17 years old. Here are some important driving rules to follow in the UK",
+    "rules": """
+1.	Seatbelts: It is mandatory for the driver and all passengers to wear seat belts at all times while the vehicle is in motion.
+2.	Speed Limits: Speed limits are clearly marked on UK roads and can vary depending on the type of road, weather conditions, and time of day. The national speed limit for cars and motorcycles on single carriageway roads is 60 mph, while the limit for dual carriageway and motorways is 70 mph.
+3.	Traffic Signals: Traffic lights in the UK follow the standard red, amber, and green system. When approaching a traffic light, it is important to pay attention to the signal and obey it accordingly.
+4.	Roundabouts: Roundabouts are a common feature of UK roads, and they are used to control traffic flow at intersections. When entering a roundabout, you must give way to traffic already on the roundabout.
+5.	Drinking and Driving: The legal limit for blood alcohol content in the UK is 80 milligrams of alcohol per 100 millilitres of blood. It is illegal to drive while under the influence of alcohol or drugs.
+6.	Mobile Phones: It is illegal to use a hand-held mobile phone while driving in the UK. You must use a hands-free device if you need to make a call while driving.
+7.	Parking: Parking is allowed on most UK roads, but it is important to pay attention to parking signs and restrictions to avoid receiving a fine or having your car towed away.
+8.	Pedestrian Crossings: Pedestrian crossings in the UK are marked with distinctive black and white stripes on the road. Drivers must stop if someone is waiting to cross or if someone has already stepped onto the crossing.
+"""
+  }
+];
+
+List<Map<String, dynamic>> airportRules = [
+  {
+    "description":
+        "The United Kingdom has several rules and regulations related to airports that are designed to ensure the safety and security of passengers and the wider public. Some of the key airport rules in the UK",
+    "rules": """
+1.	Security screening: Passengers and their belongings are subject to security screening before boarding a flight. This includes walking through metal detectors or full-body scanners, having their bags and personal items scanned, and potentially undergoing additional checks such as a pat-down search or a swab test for explosive residue.
+2.	Prohibited items: There are restrictions on what items passengers are allowed to bring on board a plane, both in their carry-on and checked baggage. These rules are designed to prevent dangerous items such as weapons, explosives, and liquids over a certain volume from being brought onto the plane.
+3.	Passport control: Passengers entering or leaving the UK must go through passport control, where their identity and travel documents are checked. Non-UK citizens may also be required to provide additional documentation, such as a visa or proof of onward travel.
+4.	Quarantine measures: The UK has introduced quarantine measures for passengers arriving from certain countries or regions, particularly in response to the COVID-19 pandemic. This may include mandatory testing or quarantine periods, depending on the circumstances.
+5.	Airport facilities: Airports in the UK provide a range of facilities for passengers, such as restaurants, shops, and rest areas. There are also facilities for passengers with disabilities or special needs, such as accessible toilets and lifts.
+"""
+  }
+];
