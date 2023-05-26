10d7d13c674e60d87a12253a8aa5f44715055ec8
H-R-S <369haris@gmail.com>
2023-05-26 21:15:47 +0500

    New dummy data added | Job Timings

---
+++ lib/resources/data/job_timings.dart
@@ -0,0 +1,12 @@
+List<Map<String, dynamic>> jobTimings= [
+  {
+    "description":
+        "Foreign students in the UK are generally allowed to work part-time while they study, but there are restrictions on the number of hours they can work. The exact job timings will depend on a number of factors, including the student's visa status and the type of job they are seeking.",
+    "rules": """
+1.	Tier 4 visa holders: If you are in the UK on a Tier 4 student visa, you are generally allowed to work up to 20 hours per week during term-time and full-time during vacation periods.
+2.	Short-term study visa holders: If you are in the UK on a short-term study visa, you are generally not allowed to work while you study.
+3.	Postgraduate students: If you are a postgraduate student, you may be eligible for additional work rights, such as working as a teaching or research assistant.
+4.	Work restrictions: There are some restrictions on the types of jobs that foreign students can work in, such as jobs that involve manual labor, and work that is considered self-employed.
+"""
+  }
+];
