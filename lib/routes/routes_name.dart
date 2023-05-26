39beb6eee7f0973241747ae8e0bcdff278d43c67
H-R-S <369haris@gmail.com>
2023-05-26 21:16:36 +0500

    New routes added | Routes Name

--- lib/routes/routes_name.dart
+++ lib/routes/routes_name.dart
@@ -18,9 +18,17 @@ class RoutesName {
 
   static const String weather = "weather_screen";
 
+  static const String airportRules = "airport_rules_screen";
+
+  static const String drivingRules = "driving_rules_screen";
+
+  static const String transportRules = "transport_rules_screen";
+
   static const String destinations = "destinations_screen";
 
   static const String location = "location_screen";
+
+  static const String jobTimings = "job_timings_screen";
 
   static const String currency = "currency_screen";
 
