d82d306cc9799a43a5b3f71557cd00f3682584de
H-R-S <369haris@gmail.com>
2023-05-27 20:21:20 +0500

    New dummy data added | FAQ

---
+++ lib/resources/data/faq.dart
@@ -0,0 +1,22 @@
+List<Map<String, dynamic>> faq = [
+  {
+    "question": "How can I search for specific destinations or attractions?",
+    "answer": "Use the search bar within the app to enter the name of the destination or attraction you're looking for, and GuideWay will provide relevant results."
+  },
+  {
+    "question": "Can I get real-time weather updates for my destination?",
+    "answer": "Yes, GuideWay provides real-time weather forecasts for various locations. Access the weather section within the app to get the latest updates."
+  },
+  {
+    "question": "How accurate is the Qibla direction feature?",
+    "answer": "The Qibla direction feature in GuideWay utilizes precise calculations and your device's sensors to provide accurate Qibla direction based on your location."
+  },
+  {
+    "question": "Is there a feedback system in place to provide suggestions or report issues?",
+    "answer": "Yes, GuideWay values user feedback. You can provide feedback, report issues, or make suggestions through the app's \"Feedback\" section."
+  },
+  {
+    "question": "How can I find popular tourist attractions in a specific city?",
+    "answer": "GuideWay provides a \"Top Attractions\" section where you can explore popular tourist spots in various cities around the world."
+  }
+];
