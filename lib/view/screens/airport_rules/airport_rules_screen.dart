243128083ea933dc79d289c390f7dc75e8b4a2c9
H-R-S <369haris@gmail.com>
2023-05-27 20:23:24 +0500

    Screen UI updated | Airport Rules Screen

--- lib/view/screens/airport_rules/airport_rules_screen.dart
+++ lib/view/screens/airport_rules/airport_rules_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:guide_way/resources/data/rules.dart';
+import 'package:provider/provider.dart';
+import '../../../theme/theme_provider.dart';
 import '../../widgets/app_bar/my_app_bar.dart';
 
 class AirportRulesScreen extends StatelessWidget {
@@ -9,7 +11,14 @@ class AirportRulesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const style = TextStyle(fontSize: 18, fontWeight: FontWeight.normal);
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    bool isDark = themeProvider.currentTheme == ThemeData.dark();
+
+    final style = TextStyle(
+        color: isDark ? Colors.white : null,
+        fontSize: 18,
+        fontWeight: FontWeight.normal);
 
     return Scaffold(
         appBar: MyAppBar(scaffoldKey, context, title: "Airport Rules"),
