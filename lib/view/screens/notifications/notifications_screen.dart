02b8e4276547adef0bc026c5ff280a89019c2cb5
H-R-S <369haris@gmail.com>
2023-05-28 23:55:41 +0500

    Screen UI Updated | Notification Screen

--- lib/view/screens/notifications/notifications_screen.dart
+++ lib/view/screens/notifications/notifications_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../../theme/theme_provider.dart';
 import '../../widgets/app_bar/my_app_bar.dart';
 
 class NotificationsScreen extends StatelessWidget {
@@ -8,8 +10,16 @@ class NotificationsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    bool isDark = themeProvider.currentTheme == ThemeData.dark();
+
     return Scaffold(
-      appBar: MyAppBar(scaffoldKey, context, title: "Notifications"),
-    );
+        appBar: MyAppBar(scaffoldKey, context, title: "Notifications"),
+        body: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text("No notification at the moment",
+                style: TextStyle(
+                    color: isDark ? Colors.white : null, fontSize: 18))));
   }
 }
