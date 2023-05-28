2a177d98bf9b6827e4713a6303a3ca53d8df752d
H-R-S <369haris@gmail.com>
2023-05-28 23:58:59 +0500

    Widget UI Updated | Forgot Dialog Widget

--- lib/view/widgets/dialog/forgot_dialog.dart
+++ lib/view/widgets/dialog/forgot_dialog.dart
@@ -1,31 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../../theme/theme_provider.dart';
 
 class ForgotDialog extends StatelessWidget {
   const ForgotDialog({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-        padding: EdgeInsets.all(20),
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    bool isDark = themeProvider.currentTheme == ThemeData.dark();
+
+    return Padding(
+        padding: const EdgeInsets.all(20),
         child: SizedBox(
             width: double.infinity,
             child: Column(mainAxisSize: MainAxisSize.min, children: [
-              CircleAvatar(
+              const CircleAvatar(
                   radius: 40,
                   backgroundColor: Colors.blue,
                   child: Icon(Icons.mark_email_read_outlined,
                       size: 50, color: Colors.white)),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               Text("Check your email",
-                  style:
-                      TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-              SizedBox(height: 20),
-              Text(
-                  "We have sent password recovery instruction to your email",
+                  style: TextStyle(
+                      color: isDark ? Colors.white : null,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20)),
+              const SizedBox(height: 20),
+              Text("We have sent password recovery instruction to your email",
                   textAlign: TextAlign.center,
                   style: TextStyle(
                       fontWeight: FontWeight.normal,
-                      color: Colors.black54,
+                      color: isDark ? Colors.white70 : Colors.black54,
                       fontSize: 18))
             ])));
   }
