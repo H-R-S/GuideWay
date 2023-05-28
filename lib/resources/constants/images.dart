aa415a89d78f6fe44d9d1e1c1185e56ecefd63bb
H-R-S <369haris@gmail.com>
2023-05-28 23:53:30 +0500

    New image added | Image Constants

--- lib/resources/constants/images.dart
+++ lib/resources/constants/images.dart
@@ -10,3 +10,5 @@ const String cambridgeUniversityImage = "assets/images/cambridge_university_imag
 const String imperialCollegeUniversityImage = "assets/images/imperial_college_university_image.jpg";
 const String oxfordUniversityImage = "assets/images/oxford_university_image.jpg";
 const String universityCollegeImage = "assets/images/university_college_image.jpg";
+
+const String emptyImage = "assets/images/empty_image.jpg";
