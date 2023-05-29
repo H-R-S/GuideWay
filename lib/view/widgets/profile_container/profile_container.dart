efff241c60a676651fe79ce591a285526e39053b
H-R-S <369haris@gmail.com>
2023-05-29 22:47:08 +0500

    Widget UI Updated | ProfileContainer Widget

--- lib/view/widgets/profile_container/profile_container.dart
+++ lib/view/widgets/profile_container/profile_container.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../../../resources/constants/icons.dart';
 import '../../../resources/constants/style.dart';
+import '../../../theme/theme_provider.dart';
 
 class ProfileContainer extends StatelessWidget {
   final String name, userName, email;
@@ -15,39 +17,48 @@ class ProfileContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const style = TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
+    final themeProvider = Provider.of<ThemeProvider>(context);
 
-    return Container(
-        margin: const EdgeInsets.all(20),
-        padding: const EdgeInsets.all(20),
-        width: double.infinity,
-        decoration: BoxDecoration(
-            color: primaryLight,
-            boxShadow: const [
-              BoxShadow(
-                  color: Colors.grey,
-                  spreadRadius: 1,
-                  blurRadius: 5,
-                  offset: Offset(0, 4))
-            ],
-            borderRadius: BorderRadius.circular(20)),
-        child: Row(children: [
-          const CircleAvatar(
-              radius: 40,
-              backgroundColor: Colors.white,
-              backgroundImage: AssetImage(personIcon)),
-          const SizedBox(width: 20),
-          FittedBox(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                Text(name, style: style),
-                const SizedBox(height: 5),
-                Text(userName,
-                    style: style.copyWith(fontWeight: FontWeight.normal)),
-                const SizedBox(height: 5),
-                Text(email, style: style)
-              ]))
-        ]));
+    bool isDark = themeProvider.currentTheme == ThemeData.dark();
+
+    final style = TextStyle(
+        color: isDark ? Colors.white : null,
+        fontSize: 18,
+        fontWeight: FontWeight.bold);
+
+    return InkWell(
+        onTap: onTap,
+        child: Container(
+            margin: const EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
+            width: double.infinity,
+            decoration: BoxDecoration(
+                color: isDark ? Color(0xff222831) : primaryLight,
+                boxShadow: [
+                  BoxShadow(
+                      color: isDark ? Colors.black12 : Colors.grey,
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: const Offset(0, 4))
+                ],
+                borderRadius: BorderRadius.circular(20)),
+            child: Row(children: [
+              const CircleAvatar(
+                  radius: 40,
+                  backgroundColor: Colors.white,
+                  backgroundImage: AssetImage(personIcon)),
+              const SizedBox(width: 20),
+              FittedBox(
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                    Text(name, style: style),
+                    const SizedBox(height: 5),
+                    Text(userName,
+                        style: style.copyWith(fontWeight: FontWeight.normal)),
+                    const SizedBox(height: 5),
+                    Text(email, style: style)
+                  ]))
+            ])));
   }
 }
