f72314e4bc6692788955dcd6746a7cb58fc594fb
H-R-S <369haris@gmail.com>
2023-05-29 22:44:51 +0500

    Widget UI Updated | SignInButton Widget

--- lib/view/widgets/button/sign_in_button.dart
+++ lib/view/widgets/button/sign_in_button.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:guide_way/routes/routes_name.dart';
+import 'package:provider/provider.dart';
 import '../../../resources/constants/style.dart';
+import '../../../theme/theme_provider.dart';
 
 class SignInButton extends StatelessWidget {
   const SignInButton({super.key});
 
   @override
   Widget build(BuildContext context) {
+    
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    bool isDark = themeProvider.currentTheme == ThemeData.dark();
+    
     return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-      const Text("Already have an account? ", style: TextStyle(fontSize: 16)),
+     Text("Already have an account? ", style: TextStyle(
+        color: isDark ? Colors.white : null,
+        fontSize: 16)),
       TextButton(
           onPressed: () {
             Navigator.pushReplacementNamed(context, RoutesName.login);
