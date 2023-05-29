58c370b403136dee9c91223f464852a00c4e9e91
H-R-S <369haris@gmail.com>
2023-05-29 22:46:34 +0500

    Widget UI Updated | InfoContainer Widget

--- lib/view/widgets/info_container/info_container.dart
+++ lib/view/widgets/info_container/info_container.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:guide_way/resources/constants/style.dart';
+import 'package:provider/provider.dart';
+
+import '../../../theme/theme_provider.dart';
 
 class InfoContainer extends StatelessWidget {
   final String title;
@@ -15,18 +19,22 @@ class InfoContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    bool isDark = themeProvider.currentTheme == ThemeData.dark();
+
     return InkWell(
         onTap: isDisable ? null : onTap,
         child: Container(
             padding: const EdgeInsets.all(20),
             decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: const [
+                color: isDark ? dark : Colors.white,
+                boxShadow: [
                   BoxShadow(
-                      color: Colors.grey,
+                      color: isDark ? Colors.black12 : Colors.grey,
                       spreadRadius: 1,
                       blurRadius: 5,
-                      offset: Offset(0, 4))
+                      offset: const Offset(0, 4))
                 ],
                 borderRadius: BorderRadius.circular(20)),
             child: FittedBox(
@@ -35,8 +43,10 @@ class InfoContainer extends StatelessWidget {
               const SizedBox(height: 10),
               Text(title,
                   textAlign: TextAlign.center,
-                  style: const TextStyle(
-                      fontSize: 18, fontWeight: FontWeight.bold))
+                  style: TextStyle(
+                      color: isDark ? Colors.white : null,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold))
             ]))));
   }
 }
