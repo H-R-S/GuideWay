60f055df7d3b254d1f4298590584a0de2bebb730
H-R-S <369haris@gmail.com>
2023-05-29 22:45:11 +0500

    Widget UI Updated | SignUpButton Widget

--- lib/view/widgets/button/sign_up_button.dart
+++ lib/view/widgets/button/sign_up_button.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:guide_way/routes/routes_name.dart';
+import 'package:provider/provider.dart';
 import '../../../resources/constants/style.dart';
+import '../../../routes/routes_name.dart';
+import '../../../theme/theme_provider.dart';
 
 class SignUpButton extends StatelessWidget {
   const SignUpButton({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    bool isDark = themeProvider.currentTheme == ThemeData.dark();
+
     return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-      const Text("Don't have an account? ", style: TextStyle(fontSize: 16)),
+      Text("Don't have an account? ",
+          style: TextStyle(color: isDark ? Colors.white : null, fontSize: 16)),
       TextButton(
           onPressed: () {
             Navigator.pushNamed(context, RoutesName.signup);
