8d4c749ed0c40715588feda92a630db92eb461ea
H-R-S <369haris@gmail.com>
2023-05-29 22:48:51 +0500

    Widget UI Updated | WeatherHeader Widget

--- lib/view/widgets/weather_header/weather_header.dart
+++ lib/view/widgets/weather_header/weather_header.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../theme/theme_provider.dart';
 
 class WeatherHeader extends StatelessWidget {
   final String city, temperature, currentWeather;
@@ -11,7 +14,13 @@ class WeatherHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const style = TextStyle(fontSize: 25, fontWeight: FontWeight.bold);
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    bool isDark = themeProvider.currentTheme == ThemeData.dark();
+
+    final style = TextStyle(
+      color: isDark ? Colors.white : null,
+      fontSize: 25, fontWeight: FontWeight.bold);
 
     return Column(children: [
       Text(city, style: style),
