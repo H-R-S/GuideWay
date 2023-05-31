d1782e018022d2c027aa2635153987c6b2db389b
H-R-S <369haris@gmail.com>
2023-05-31 22:46:00 +0500

    Auth functions added (Email, Facebook, Google & Twitter) | Auth View Model

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:guide_way/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'routes/routes.dart';
-import 'routes/routes_name.dart';
+import 'theme/theme_provider.dart';
+import 'view/screens/login/login_screen.dart';
+import 'view/screens/splash/splash_screen.dart';
+import 'view/widgets/snack_bar/my_snack_bar.dart';
+import 'view_models/auth/auth_view_model.dart';
+import 'view_models/user/user_view_model.dart';
 
 int? initScreen;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   SharedPreferences preferences = await SharedPreferences.getInstance();
   initScreen = preferences.getInt('initScreen');
   await preferences.setInt('initScreen', 1);
@@ -31,6 +38,8 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MultiProvider(
         providers: [
+          ChangeNotifierProvider(create: (context) => AuthViewModel()),
+          ChangeNotifierProvider(create: (context) => UserViewModel()),
           ChangeNotifierProvider(create: (context) => ThemeProvider())
         ],
         child: ChangeNotifierProvider(
@@ -45,9 +54,23 @@ class _MyAppState extends State<MyApp> {
                   debugShowCheckedModeBanner: false,
                   title: 'GuideWay',
                   onGenerateRoute: Routes.generateRoute,
-                  initialRoute: initScreen == 0 || initScreen == null
-                      ? RoutesName.onBoarding
-                      : RoutesName.home);
+                  home: StreamBuilder<User?>(
+                      stream: FirebaseAuth.instance.authStateChanges(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const CircularProgressIndicator();
+                        } else if (snapshot.hasError) {
+                          return MySnackBar(context, snapshot.error.toString());
+                        } else if (snapshot.hasData) {
+                          return const SplashScreen();
+                        } else {
+                          return LoginScreen();
+                        }
+                      }));
+              // initialRoute: initScreen == 0 || initScreen == null
+              //     ? RoutesName.onBoarding
+              //     : RoutesName.home);
             }));
   }
 }
