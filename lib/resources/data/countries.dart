7b706186c06eba8c7a4e6cdc4acc84e6b7bdbb82
H-R-S <369haris@gmail.com>
2023-05-31 22:40:57 +0500

    Countries data updated | Countries

--- lib/resources/data/countries.dart
+++ lib/resources/data/countries.dart
@@ -1,64 +1,65 @@
+import 'package:guide_way/models/country/country_model.dart';
 import '../constants/flags.dart';
 
-List<Map<String, dynamic>> countries = [
-  {
-    "id": 1,
-    "name": "USA",
-    "flag": usaFlag,
-    "dialingCode": "+1",
-  },
-  {
-    "id": 11,
-    "name": "Canada",
-    "flag": canadaFlag,
-    "dialingCode": "+1",
-  },
-  {
-    "id": 33,
-    "name": "France",
-    "flag": franceFlag,
-    "dialingCode": "+33",
-  },
-  {
-    "id": 44,
-    "name": "UK",
-    "flag": ukFlag,
-    "dialingCode": "+44",
-  },
-  {
-    "id": 46,
-    "name": "Sweden",
-    "flag": swedenFlag,
-    "dialingCode": "+46",
-  },
-  {
-    "id": 47,
-    "name": "Norway",
-    "flag": norwayFlag,
-    "dialingCode": "+47",
-  },
-  {
-    "id": 49,
-    "name": "Germany",
-    "flag": germanyFlag,
-    "dialingCode": "+49",
-  },
-  {
-    "id": 61,
-    "name": "Australia",
-    "flag": australiaFlag,
-    "dialingCode": "+61",
-  },
-  {
-    "id": 81,
-    "name": "Japan",
-    "flag": japanFlag,
-    "dialingCode": "+81",
-  },
-  {
-    "id": 82,
-    "name": "South Korea",
-    "flag": southKoreaFlag,
-    "dialingCode": "+82",
-  }
+List<CountryModel> countries = [
+  CountryModel(
+    id: 1,
+    name: "USA",
+    flag: usaFlag,
+    dialingCode: "+1",
+  ),
+  CountryModel(
+    id: 11,
+    name: "Canada",
+    flag: canadaFlag,
+    dialingCode: "+1",
+  ),
+  CountryModel(
+    id: 33,
+    name: "France",
+    flag: franceFlag,
+    dialingCode: "+33",
+  ),
+  CountryModel(
+    id: 44,
+    name: "UK",
+    flag: ukFlag,
+    dialingCode: "+44",
+  ),
+  CountryModel(
+    id: 46,
+    name: "Sweden",
+    flag: swedenFlag,
+    dialingCode: "+46",
+  ),
+  CountryModel(
+    id: 47,
+    name: "Norway",
+    flag: norwayFlag,
+    dialingCode: "+47",
+  ),
+  CountryModel(
+    id: 49,
+    name: "Germany",
+    flag: germanyFlag,
+    dialingCode: "+49",
+  ),
+  CountryModel(
+    id: 61,
+    name: "Australia",
+    flag: australiaFlag,
+    dialingCode: "+61",
+  ),
+  CountryModel(
+    id: 81,
+    name: "Japan",
+    flag: japanFlag,
+    dialingCode: "+81",
+  ),
+  CountryModel(
+    id: 82,
+    name: "South Korea",
+    flag: southKoreaFlag,
+    dialingCode: "+82",
+  )
 ];
