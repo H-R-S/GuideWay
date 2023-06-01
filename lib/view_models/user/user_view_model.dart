be3adcce4a4e82c3ab09f36a909f4280c1eb21b2
H-R-S <369haris@gmail.com>
2023-06-01 23:47:25 +0500

    getUser function added | User View Model

--- lib/view_models/user/user_view_model.dart
+++ lib/view_models/user/user_view_model.dart
@@ -2,9 +2,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import '../../models/user/user_model.dart';
+import '../../routes/routes_name.dart';
+import '../../view/widgets/snack_bar/my_snack_bar.dart';
 
 class UserViewModel with ChangeNotifier {
-  User? get currentUser => FirebaseAuth.instance.currentUser;
+  User? get user => FirebaseAuth.instance.currentUser;
 
   bool _loading = false;
 
@@ -15,13 +18,34 @@ class UserViewModel with ChangeNotifier {
     notifyListeners();
   }
 
-  updateUserCountry(int countryId) async {
+  updateUserCountry(BuildContext context, int countryId) async {
     setLoading(true);
     final userCollection =
-        FirebaseFirestore.instance.collection("users").doc(currentUser!.uid);
+        FirebaseFirestore.instance.collection("users").doc(user!.uid);
     saveCountryId(countryId);
     await userCollection.update({"countryId": countryId}).then((value) {
       setLoading(false);
+      MySnackBar(context, "Country updated");
+      Navigator.pushReplacementNamed(context, RoutesName.home);
+    });
+  }
+
+  updateUserInfo(BuildContext context, UserModel userModel) async {
+    setLoading(true);
+
+    final userCollection =
+        FirebaseFirestore.instance.collection("users").doc(user!.uid);
+
+    await userCollection.update({
+      "fullName": userModel.fullName,
+      "username": userModel.username,
+      "dob": userModel.dob
+    }).then((value) {
+      setLoading(false);
+      MySnackBar(context, "Info Updated!");
+    }).onError((error, stackTrace) {
+      setLoading(false);
+      MySnackBar(context, error.toString());
     });
   }
 
@@ -45,14 +69,15 @@ class UserViewModel with ChangeNotifier {
     return countryId;
   }
 
-  Future<int?> getUserCountryId() async {
-    await FirebaseFirestore.instance
-        .collection("users")
-        .doc(currentUser!.uid)
-        .get()
-        .then((value) {
-      return value["countryId"];
-    });
-    return null;
+  Future<UserModel?> getUser() async {
+    final docUser =
+        FirebaseFirestore.instance.collection("users").doc(user!.uid);
+
+    final userSnapshot = await docUser.get();
+
+    if (userSnapshot.exists) {
+      return UserModel.fromJson(userSnapshot.data()!);
+    }
+    return UserModel();
   }
 }
