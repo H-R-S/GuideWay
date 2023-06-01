6192bfd3d3b4983bbcf1f39d4847e192af4614df
H-R-S <369haris@gmail.com>
2023-06-01 23:45:35 +0500

    User update profile function added | Profile Screen

--- lib/view/screens/profile/profile_screen.dart
+++ lib/view/screens/profile/profile_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:guide_way/models/user/user_model.dart';
+import 'package:guide_way/view_models/user/user_view_model.dart';
+import 'package:provider/provider.dart';
 import '../../widgets/app_bar/my_app_bar.dart';
 import '../../widgets/button/my_elevated_button.dart';
 import '../../widgets/text_field/my_text_field.dart';
@@ -19,23 +22,60 @@ class ProfileScreen extends StatelessWidget {
         appBar: MyAppBar(scaffoldKey, context, title: "Profile"),
         body: Padding(
             padding: const EdgeInsets.all(20),
-            child: Column(children: [
-              MyTextField(
-                  header: "Username",
-                  controller: usernameController,
-                  hint: "Username"),
-              MyTextField(
-                  header: "Full Name",
-                  controller: fullnameController,
-                  hint: "Full Name"),
-              MyTextField(
-                  header: "Email", controller: emailController, hint: "Email"),
-              MyTextField(
-                  header: "DOB",
-                  controller: dobController,
-                  hint: "Date of Birth"),
-              const SizedBox(height: 40),
-              MyElevatedButton(title: "Update", onTap: () {})
-            ])));
+            child: Consumer<UserViewModel>(builder: (context, value, child) {
+              return FutureBuilder<UserModel?>(
+                  future: value.getUser(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      debugPrint(snapshot.error.toString());
+                      return Container();
+                    } else if (snapshot.hasData) {
+                      final user = snapshot.data!;
+
+                      usernameController.value = usernameController.value
+                          .copyWith(text: user.username);
+                      emailController.value =
+                          emailController.value.copyWith(text: user.email);
+                      emailController.value =
+                          emailController.value.copyWith(text: user.email);
+                      dobController.value =
+                          dobController.value.copyWith(text: user.dob);
+
+                      return Column(children: [
+                        MyTextField(
+                            header: "Username",
+                            controller: usernameController,
+                            hint: "Username"),
+                        MyTextField(
+                            header: "Full Name",
+                            controller: fullnameController,
+                            hint: "Full Name"),
+                        MyTextField(
+                            isReadable: true,
+                            header: "Email",
+                            controller: emailController,
+                            hint: "Email"),
+                        MyTextField(
+                            header: "DOB",
+                            controller: dobController,
+                            hint: "Date of Birth"),
+                        const SizedBox(height: 40),
+                        MyElevatedButton(
+                            isLoading: value.loading,
+                            title: "Update",
+                            onTap: () {
+                              value.updateUserInfo(
+                                  context,
+                                  UserModel(
+                                      username: usernameController.text.trim(),
+                                      fullName: fullnameController.text.trim(),
+                                      dob: dobController.text.trim()));
+                            })
+                      ]);
+                    } else {
+                      return const CircularProgressIndicator();
+                    }
+                  });
+            })));
   }
 }
