f428bd61620c21ccf41b71b1e791636b12bcaec1
H-R-S <369haris@gmail.com>
2023-06-02 23:57:35 +0500

    New URL's added | AppURL

---
+++ lib/resources/url/app_url.dart
@@ -0,0 +1,8 @@
+class AppURL {
+  
+  static var baseTranslateUrl = "https://text-translator2.p.rapidapi.com";
+
+  static var getLanguagesEndPoint = "$baseTranslateUrl/getLanguages";
+
+  static var getTranslationEndPoint = "$baseTranslateUrl/translate";
+}
