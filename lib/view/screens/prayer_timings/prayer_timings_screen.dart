d955ab05ddde6ecbd1b5ebf8a1db80b0806c3884
H-R-S <369haris@gmail.com>
2023-06-03 23:59:32 +0500

    Screen UI updated | PrayerTimings Screen

--- lib/view/screens/prayer_timings/prayer_timings_screen.dart
+++ lib/view/screens/prayer_timings/prayer_timings_screen.dart
@@ -1,31 +1,89 @@
 import 'package:flutter/material.dart';
-import 'package:guide_way/resources/data/prayer_time.dart';
-import 'package:guide_way/view/widgets/prayer_time_container/prayer_time_container.dart';
+import 'package:provider/provider.dart';
+import '../../../data/responses/status.dart';
+import '../../../view_models/prayer/prayer_view_model.dart';
 import '../../widgets/app_bar/my_app_bar.dart';
+import '../../widgets/prayer_time_container/prayer_time_container.dart';
 
-class PrayerTimingsScreen extends StatelessWidget {
-  PrayerTimingsScreen({super.key});
+class PrayerTimingsScreen extends StatefulWidget {
+  const PrayerTimingsScreen({super.key});
 
+  @override
+  State<PrayerTimingsScreen> createState() => _PrayerTimingsScreenState();
+}
+
+class _PrayerTimingsScreenState extends State<PrayerTimingsScreen> {
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+
+  PrayerViewModel prayerViewModel = PrayerViewModel();
+
+  @override
+  void initState() {
+    prayerViewModel.getPrayerDetails(context);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: MyAppBar(scaffoldKey, context, title: "Prayer Timings"),
-        body: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(children: [
-              const PrayerTimeContainer(isHeader: true),
-              ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: prayerTime.length,
-                  itemBuilder: (context, index) {
-                    return PrayerTimeContainer(
-                        name: prayerTime[index]["name"],
-                        azanTime: prayerTime[index]["azanTime"],
-                        iqamaTime: prayerTime[index]["iqamaTime"]);
-                  })
-            ])));
+        body: ChangeNotifierProvider<PrayerViewModel>(
+            create: (context) => prayerViewModel,
+            builder: (context, snapshot) {
+              return SingleChildScrollView(
+                child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Consumer<PrayerViewModel>(
+                        builder: (context, value, child) {
+                      switch (value.prayerDetails.status) {
+                        case Status.ERROR:
+                          debugPrint(value.prayerDetails.message);
+                          return Container();
+              
+                        case Status.COMPLETED:
+                          final prayerTimings =
+                              value.prayerDetails.data!.data!.timings!;
+              
+                          final prayerDate =
+                              value.prayerDetails.data!.data!.date!;
+
+                          return Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const PrayerTimeContainer(isInfoHeader: true),
+                                PrayerTimeContainer(
+                                    title: "Date :", value: prayerDate.readable),
+                                PrayerTimeContainer(
+                                    title: "Date Hijri :",
+                                    value: prayerDate.hijri!.date),
+                                PrayerTimeContainer(
+                                    title: "Month Hijri :",
+                                    value:
+                                        "${prayerDate.hijri!.month!.en} ( ${prayerDate.hijri!.month!.ar} )"),
+                                const PrayerTimeContainer(isHeader: true),
+                                PrayerTimeContainer(
+                                    title: "Fajr", value: prayerTimings.fajr),
+                                PrayerTimeContainer(
+                                    title: "Sunrise",
+                                    value: prayerTimings.sunrise),
+                                PrayerTimeContainer(
+                                    title: "Dhuhr", value: prayerTimings.dhuhr),
+                                PrayerTimeContainer(
+                                    title: "Asr", value: prayerTimings.asr),
+                                PrayerTimeContainer(
+                                    title: "Sunset", value: prayerTimings.sunset),
+                                PrayerTimeContainer(
+                                    title: "Maghrib",
+                                    value: prayerTimings.maghrib),
+                                PrayerTimeContainer(
+                                    title: "Isha", value: prayerTimings.isha)
+                              ]);
+              
+                        default:
+                          return const Center(child: CircularProgressIndicator());
+                      }
+                    })),
+              );
+            }));
   }
 }
