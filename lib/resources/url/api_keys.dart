56bcfd140ac93ac8f3c4682f896cb01f3258097c
H-R-S <369haris@gmail.com>
2023-06-03 23:58:36 +0500

    New URL's added | AppURL

--- lib/resources/url/api_keys.dart
+++ lib/resources/url/api_keys.dart
@@ -1,6 +1,7 @@
 class ApiKeys {
 
-  // 100,000 character limit / month
-  static var translateApiKey =
+  // 100,000 character limit / month (translate)
+  // Unlimited (Currency)  
+  static var rapidApiKey =
       "d548581ba8msha07806db1d821fdp1dacfcjsn9ae339ca33a2";
 }
--- lib/resources/url/app_url.dart
+++ lib/resources/url/app_url.dart
@@ -1,8 +1,19 @@
 class AppURL {
   
+  // Translate API's
   static var baseTranslateUrl = "https://text-translator2.p.rapidapi.com";
 
   static var getLanguagesEndPoint = "$baseTranslateUrl/getLanguages";
-
   static var getTranslationEndPoint = "$baseTranslateUrl/translate";
+
+  // Currency API's 
+  static var baseCurrencyUrl = "https://currency-converter-pro1.p.rapidapi.com";
+
+  static var getCurrenciesEndPoint = "$baseCurrencyUrl/currencies";
+  static var getCurrencyConvertEndPoint = "$baseCurrencyUrl/convert";
+
+  // Prayer's Timing API
+  static var basePrayerUrl = "https://api.aladhan.com";
+
+  static var getPrayerTimingsEndPoint = "$basePrayerUrl/v1/timings";
 }
