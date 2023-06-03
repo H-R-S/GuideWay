25fd53a99465143181510db6857e86a43f7b2161
H-R-S <369haris@gmail.com>
2023-06-03 23:59:10 +0500

    Screen UI updated | Location Screen

--- lib/view/screens/location/location_screen.dart
+++ lib/view/screens/location/location_screen.dart
@@ -1,6 +1,11 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
+import '../../../models/country/country_model.dart';
+import '../../../models/user/user_model.dart';
+import '../../../resources/data/countries.dart';
+import '../../../view_models/user/user_view_model.dart';
 import '../../widgets/app_bar/my_app_bar.dart';
 
 class LocationScreen extends StatefulWidget {
@@ -25,12 +30,50 @@ class _LocationScreenState extends State<LocationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: MyAppBar(scaffoldKey, context, title: "Location"),
-        body: GoogleMap(
-            mapType: MapType.normal,
-            initialCameraPosition: _kGooglePlex,
-            onMapCreated: (GoogleMapController controller) {
-              _controller.complete(controller);
-            }
-        ));
+        body: Consumer<UserViewModel>(builder: (context, value, child) {
+          return FutureBuilder<UserModel?>(
+              future: value.getUser(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return GoogleMap(
+                      mapType: MapType.normal,
+                      initialCameraPosition: _kGooglePlex,
+                      onMapCreated: (GoogleMapController controller) {
+                        _controller.complete(controller);
+                      });
+                }
+                if (snapshot.hasData) {
+                  final user = snapshot.data!;
+
+                  CountryModel? userCountry = countries
+                      .firstWhere((country) => country.id == user.countryId);
+
+                  CameraPosition userLoc = CameraPosition(
+                    target: LatLng(userCountry.latitude!.toDouble(),
+                        userCountry.longitude!.toDouble()),
+                    zoom: 14.4746,
+                  );
+
+                  return GoogleMap(
+                      mapType: MapType.normal,
+                      initialCameraPosition: userLoc,
+                      markers: {
+                        Marker(
+                            markerId: const MarkerId("markerId"),
+                            infoWindow: InfoWindow(
+                                title:
+                                    "${userCountry.capital} ,${userCountry.name}"),
+                            icon: BitmapDescriptor.defaultMarker,
+                            position: LatLng(userCountry.latitude!.toDouble(),
+                                userCountry.longitude!.toDouble()))
+                      },
+                      onMapCreated: (GoogleMapController controller) {
+                        _controller.complete(controller);
+                      });
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              });
+        }));
   }
 }
