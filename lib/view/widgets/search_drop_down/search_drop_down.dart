e7fa071c540490c1578cb234760b6c0cc5af31b1
H-R-S <369haris@gmail.com>
2023-06-08 23:57:17 +0500

    Widget UI Updated | SearchDropDown Widget

--- lib/view/widgets/search_drop_down/search_drop_down.dart
+++ lib/view/widgets/search_drop_down/search_drop_down.dart
@@ -2,6 +2,7 @@ import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../../models/currency/currency_model.dart';
 import '../../../models/language/language_model.dart';
 import '../../../theme/theme_provider.dart';
 
@@ -94,6 +95,70 @@ class LanguageDropDown extends StatelessWidget {
             child: DropdownSearch<Languages>(
                 itemAsString: (Languages language) {
                   return "${language.name} (${language.code!.toUpperCase()})";
+                },
+                popupProps: const PopupProps.menu(
+                  showSearchBox: true,
+                  // showSelectedItems: true,
+                ),
+                items: items,
+                // selectedItem: selectedItem,
+                dropdownDecoratorProps: DropDownDecoratorProps(
+                    baseStyle: TextStyle(color: isDark ? Colors.white : null),
+                    dropdownSearchDecoration: InputDecoration(
+                        hintStyle:
+                            TextStyle(color: isDark ? Colors.white : null),
+                        hintText: hint)),
+                onChanged: onChanged),
+          ))
+    ]);
+  }
+}
+
+
+class CurrencyDropDown extends StatelessWidget {
+  final String? header;
+  final bool isRequired;
+  final String hint;
+  final List<CurrencyModel> items;
+  final CurrencyModel? selectedItem;
+  final Function(CurrencyModel?) onChanged;
+
+  const CurrencyDropDown(
+      {super.key,
+      required this.items,
+      required this.hint,
+      required this.onChanged,
+      this.selectedItem,
+      this.header,
+      this.isRequired = false});
+
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    bool isDark = themeProvider.currentTheme == ThemeData.dark();
+
+    return Column(children: [
+      if (header != null)
+        Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+          Text(header!, style: TextStyle(
+            color: isDark ? Colors.white : null,
+            fontSize: 16)),
+          const SizedBox(width: 3),
+          if (isRequired)
+            const Text("*", style: TextStyle(fontSize: 16, color: Colors.red))
+        ]),
+      const SizedBox(height: 5),
+      Container(
+          padding: const EdgeInsets.only(left: 10, right: 5, top: 5),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.grey)),
+          child: Theme(
+            data: isDark ? ThemeData.dark() : ThemeData.light(),
+            child: DropdownSearch<CurrencyModel>(
+                itemAsString: (CurrencyModel currency) {
+                  return "${currency.name} (${currency.symbol})";
                 },
                 popupProps: const PopupProps.menu(
                   showSearchBox: true,
