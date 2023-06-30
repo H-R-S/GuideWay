6c939be2211bbaa609e8ea531c54b5e45a71b6d9
H-R-S <369haris@gmail.com>
2023-06-30 15:44:53 +0500

    Screen UI updated | FeedBack Screen

--- lib/view/screens/feedback/feedback_screen.dart
+++ lib/view/screens/feedback/feedback_screen.dart
@@ -1,45 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:guide_way/view/widgets/app_bar/my_app_bar.dart';
-import 'package:guide_way/view/widgets/button/my_elevated_button.dart';
-import 'package:guide_way/view/widgets/text_field/my_text_field.dart';
+import 'package:provider/provider.dart';
+import '../../../resources/constants/style.dart';
+import '../../../view_models/feedback/feedback_view_model.dart';
+import '../../widgets/add_feedback_container/add_feedback_container.dart';
+import '../../widgets/app_bar/my_app_bar.dart';
+import '../../widgets/feedback_container/feedback_container.dart';
+import '../../widgets/loading_indicator/my_loading_indicator.dart';
 
 class FeedBackScreen extends StatelessWidget {
   FeedBackScreen({super.key});
 
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
-  final TextEditingController feedbackReasonController =
-      TextEditingController();
+  final TextEditingController reasonController = TextEditingController();
   final TextEditingController feedbackController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    final feedBackViewModel = Provider.of<FeedBackViewModel>(context);
+
     return Scaffold(
+        floatingActionButton: FloatingActionButton(
+            backgroundColor: primary,
+            onPressed: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (context) => AddFeedBackContainer(
+                      reasonController: reasonController,
+                      feedbackController: feedbackController,
+                      isLoading: feedBackViewModel.loading,
+                      onTap: () {
+                        feedBackViewModel.addFeedBack(context,
+                            reasonController.text, feedbackController.text);
+                      }));
+            },
+            child: const Icon(Icons.add)),
         appBar: MyAppBar(scaffoldKey, context, title: "FeedBack"),
         body: Padding(
             padding: const EdgeInsets.all(20),
-            child: Column(children: [
-              MyTextField(
-                  header: "FeedBack Reason",
-                  isRequired: true,
-                  isReadable: true,
-                  controller: feedbackReasonController,
-                  dropDownList: const [
-                    "I have a question",
-                    "I want to report a problem",
-                    "I want to request a feature",
-                    "I like the app because",
-                    "Other"
-                  ],
-                  hint: "Reason for FeedBack"),
-              MyTextField(
-                  header: "FeedBack",
-                  isRequired: true,
-                  controller: feedbackController,
-                  minLine: 3,
-                  hint: "Type your feedback here"),
-              const SizedBox(height: 40),
-              MyElevatedButton(title: "Submit", onTap: () {})
-            ])));
+            child:
+                Consumer<FeedBackViewModel>(builder: (context, value, child) {
+              return StreamBuilder(
+                  stream: value.getFeedBacks(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      debugPrint(snapshot.error.toString());
+                      return Container();
+                    }
+
+                    if (snapshot.hasData) {
+                      return ListView.builder(
+                          physics: const NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            final feedBack = snapshot.data![index];
+
+                            return FeedBackContainer(
+                                name: feedBack.user["fullName"],
+                                dateTime:
+                                    feedBack.timestamp!.toDate().toString(),
+                                reason: feedBack.reason ?? "",
+                                feedback: feedBack.feedback ?? "");
+                          });
+                    } else {
+                      return const MyLoadingIndicator();
+                    }
+                  });
+            })));
   }
 }
