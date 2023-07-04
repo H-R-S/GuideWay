6f9cfef8f427efa24b3025b2f35323b54d8ef501
H-R-S <369haris@gmail.com>
2023-07-04 20:14:23 +0500

    Services updated | Splash Services

--- lib/services/splash/splash_service.dart
+++ lib/services/splash/splash_service.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:guide_way/models/user/user_model.dart';
+import 'package:provider/provider.dart';
 import '../../routes/routes_name.dart';
 import '../../view_models/user/user_view_model.dart';
 
@@ -8,8 +9,11 @@ class SplashService {
   Future<UserModel?> getCountryId() => UserViewModel().getUser();
 
   void checkCountryId(BuildContext context) async {
+    final userViewModel = Provider.of<UserViewModel>(context, listen: false);
+
     getCountryId().then((value) {
-      debugPrint(value!.countryId.toString());
+      userViewModel.setCountryId(value!.countryId!);
+      debugPrint(value.countryId.toString());
       if (value.countryId == null) {
         return Navigator.of(context, rootNavigator: true)
             .pushReplacementNamed(RoutesName.country);
