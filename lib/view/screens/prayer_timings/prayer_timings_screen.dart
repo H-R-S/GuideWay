c462a3a67ae138c5f585dcfe70f0eb15ee02f90f
H-R-S <369haris@gmail.com>
2023-07-04 20:16:15 +0500

    Screen UI updated | Prayer Timings Screen

--- lib/view/screens/prayer_timings/prayer_timings_screen.dart
+++ lib/view/screens/prayer_timings/prayer_timings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:guide_way/view/widgets/loading_indicator/my_loading_indicator.dart';
 import 'package:provider/provider.dart';
 import '../../../data/responses/status.dart';
 import '../../../view_models/prayer/prayer_view_model.dart';
@@ -31,59 +32,61 @@ class _PrayerTimingsScreenState extends State<PrayerTimingsScreen> {
             create: (context) => prayerViewModel,
             builder: (context, snapshot) {
               return SingleChildScrollView(
-                child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Consumer<PrayerViewModel>(
-                        builder: (context, value, child) {
-                      switch (value.prayerDetails.status) {
-                        case Status.ERROR:
-                          debugPrint(value.prayerDetails.message);
-                          return Container();
-              
-                        case Status.COMPLETED:
-                          final prayerTimings =
-                              value.prayerDetails.data!.data!.timings!;
-              
-                          final prayerDate =
-                              value.prayerDetails.data!.data!.date!;
+                  child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Consumer<PrayerViewModel>(
+                          builder: (context, value, child) {
+                        switch (value.prayerDetails.status) {
+                          case Status.ERROR:
+                            debugPrint(value.prayerDetails.message);
+                            return Container();
 
-                          return Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const PrayerTimeContainer(isInfoHeader: true),
-                                PrayerTimeContainer(
-                                    title: "Date :", value: prayerDate.readable),
-                                PrayerTimeContainer(
-                                    title: "Date Hijri :",
-                                    value: prayerDate.hijri!.date),
-                                PrayerTimeContainer(
-                                    title: "Month Hijri :",
-                                    value:
-                                        "${prayerDate.hijri!.month!.en} ( ${prayerDate.hijri!.month!.ar} )"),
-                                const PrayerTimeContainer(isHeader: true),
-                                PrayerTimeContainer(
-                                    title: "Fajr", value: prayerTimings.fajr),
-                                PrayerTimeContainer(
-                                    title: "Sunrise",
-                                    value: prayerTimings.sunrise),
-                                PrayerTimeContainer(
-                                    title: "Dhuhr", value: prayerTimings.dhuhr),
-                                PrayerTimeContainer(
-                                    title: "Asr", value: prayerTimings.asr),
-                                PrayerTimeContainer(
-                                    title: "Sunset", value: prayerTimings.sunset),
-                                PrayerTimeContainer(
-                                    title: "Maghrib",
-                                    value: prayerTimings.maghrib),
-                                PrayerTimeContainer(
-                                    title: "Isha", value: prayerTimings.isha)
-                              ]);
-              
-                        default:
-                          return const Center(child: CircularProgressIndicator());
-                      }
-                    })),
-              );
+                          case Status.COMPLETED:
+                            final prayerTimings =
+                                value.prayerDetails.data!.data!.timings!;
+
+                            final prayerDate =
+                                value.prayerDetails.data!.data!.date!;
+
+                            return Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const PrayerTimeContainer(isInfoHeader: true),
+                                  PrayerTimeContainer(
+                                      title: "Date :",
+                                      value: prayerDate.readable),
+                                  PrayerTimeContainer(
+                                      title: "Date Hijri :",
+                                      value: prayerDate.hijri!.date),
+                                  PrayerTimeContainer(
+                                      title: "Month Hijri :",
+                                      value:
+                                          "${prayerDate.hijri!.month!.en} ( ${prayerDate.hijri!.month!.ar} )"),
+                                  const PrayerTimeContainer(isHeader: true),
+                                  PrayerTimeContainer(
+                                      title: "Fajr", value: prayerTimings.fajr),
+                                  PrayerTimeContainer(
+                                      title: "Sunrise",
+                                      value: prayerTimings.sunrise),
+                                  PrayerTimeContainer(
+                                      title: "Dhuhr",
+                                      value: prayerTimings.dhuhr),
+                                  PrayerTimeContainer(
+                                      title: "Asr", value: prayerTimings.asr),
+                                  PrayerTimeContainer(
+                                      title: "Sunset",
+                                      value: prayerTimings.sunset),
+                                  PrayerTimeContainer(
+                                      title: "Maghrib",
+                                      value: prayerTimings.maghrib),
+                                  PrayerTimeContainer(
+                                      title: "Isha", value: prayerTimings.isha)
+                                ]);
+
+                          default:
+                            return const MyLoadingIndicator();
+                        }
+                      })));
             }));
   }
 }
