5ceb7c8515ee6eae4acfc6a8c796530990c27f83
H-R-S <369haris@gmail.com>
2023-07-24 19:05:17 +0500

    Screen UI Updated | Destinations Screen

--- lib/view/screens/destinations/destinations_screen.dart
+++ lib/view/screens/destinations/destinations_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:guide_way/resources/data/destinations.dart';
-import 'package:guide_way/routes/routes.dart';
-import 'package:guide_way/view/screens/detail/detail_screen.dart';
-import 'package:guide_way/view/widgets/image_container/image_container.dart';
+import 'package:provider/provider.dart';
+import '../../../models/destination/destination.dart';
+import '../../../view_models/destination/destination_view_model.dart';
+import '../../../view_models/user/user_view_model.dart';
 import '../../widgets/app_bar/my_app_bar.dart';
+import '../../widgets/error_container/error_container.dart';
+import '../../widgets/image_container/image_container.dart';
+import '../../widgets/loading_indicator/my_loading_indicator.dart';
+import '../detail/detail_screen.dart';
 
 class DestinationsScreen extends StatelessWidget {
   DestinationsScreen({super.key});
@@ -12,28 +16,58 @@ class DestinationsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final userViewModel = Provider.of<UserViewModel>(context);
+
     return Scaffold(
-        appBar: MyAppBar(scaffoldKey, context, title: "Destionations"),
+        appBar: MyAppBar(scaffoldKey, context, title: "Destinations"),
         body: SingleChildScrollView(
             child: Padding(
                 padding: const EdgeInsets.all(20),
-                child: Column(children: [
-                  ListView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemCount: destinations.length,
-                      itemBuilder: (context, index) {
-                        return ImageContainer(
-                            name: destinations[index]["name"],
-                            description: destinations[index]["description"],
-                            image: destinations[index]["image"],
-                            onTap: () {
-                              Navigator.push(context, getPageRoute(DetailScreen(
-                                title: destinations[index]["name"],
-                                description: destinations[index]["description"]
-                              )));
-                            });
-                      })
-                ]))));
+                child: Consumer<DestinationViewModel>(
+                    builder: (context, value, child) {
+                  return StreamBuilder<List<DestinationModel>>(
+                      stream: value.getDestinations(userViewModel.countryId),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasError) {
+                          debugPrint(snapshot.error.toString());
+                          return ErrorContainer(
+                            description: snapshot.error.toString());
+
+                        } else if (snapshot.hasData) {
+                          return ListView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: snapshot.data!.length,
+                              itemBuilder: (context, index) {
+                                final destination = snapshot.data![index];
+
+                                return ImageContainer(
+                                    name: destination.name,
+                                    description: destination.description,
+                                    image: destination.thumnailImage,
+                                    onTap: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  DetailScreen(
+                                                      title: destination.name,
+                                                      description: destination
+                                                          .description,
+                                                      images:
+                                                          destination.images,
+                                                      latitude: destination
+                                                          .latitude
+                                                          .toString(),
+                                                      longitude: destination
+                                                          .longitude
+                                                          .toString())));
+                                    });
+                              });
+                        } else {
+                          return const MyLoadingIndicator();
+                        }
+                      });
+                }))));
   }
 }
